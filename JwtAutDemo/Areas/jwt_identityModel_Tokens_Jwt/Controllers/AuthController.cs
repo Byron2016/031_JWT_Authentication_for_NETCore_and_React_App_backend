faf3c39a97f8b2e238fe700cfea389bfc1298798
Byron 2016 <bgva2005@yahoo.com>
2021-12-27 11:28:05 -0500

    fix: register a new user

--- JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Controllers/AuthController.cs
+++ JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JwtAutDemo.Areas.Dtos;
+using JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Data;
+using JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,14 +9,27 @@ using System.Threading.Tasks;
 
 namespace JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Controllers
 {
-    [Route("")]
+    [Route("api")]
     [ApiController]
     public class AuthController : Controller
     {
-        [HttpGet]
-        public IActionResult Hello()
+        private readonly IUserRepository _user;
+
+        public AuthController(IUserRepository user)
         {
-            return Ok("success");
+            _user = user;
+        }
+        [HttpPost("register")]
+        public IActionResult Register(RegisterDto dto)
+        {
+            var user = new User
+            {
+                Name = dto.Name,
+                Email = dto.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(dto.Password) 
+            };
+            
+            return Created("success", _user.Create(user));
         }
     }
 }
