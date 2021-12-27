81f0010ddca1370dcc29d064fc9f219c95365fd2
Byron 2016 <bgva2005@yahoo.com>
2021-12-27 09:11:10 -0500

    fix: add user model

--- JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Models/User.cs
+++ JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Models/User.cs
@@ -5,12 +5,11 @@ using System.Threading.Tasks;
 
 namespace JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Models
 {
-    public class UserDBContext
+    public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
     }
 }
