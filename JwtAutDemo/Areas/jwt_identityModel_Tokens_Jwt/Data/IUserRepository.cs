8f6335bf1062155c78fc8fb10aab734425ba2dd0
Byron 2016 <bgva2005@yahoo.com>
2021-12-27 10:48:01 -0500

    fix: add user interfase

---
+++ JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Data/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Data
+{
+    public interface IUserRepository
+    {
+        User Create(User user);
+    }
+}
