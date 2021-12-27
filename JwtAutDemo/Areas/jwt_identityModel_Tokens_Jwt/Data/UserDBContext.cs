17fbb19e20df6bb091b00afcbc2d47d33b35d45c
Byron 2016 <bgva2005@yahoo.com>
2021-12-27 09:12:47 -0500

    fix: add 9.43 add DbContext

---
+++ JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Data/UserDBContext.cs
@@ -0,0 +1,19 @@
+﻿using JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Data
+{
+    public class UserDBContext: DbContext
+    {
+        public UserDBContext(DbContextOptions<UserDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { set; get; }
+    }
+}
