5303af2bf30ccdb6f1ef42926c46449c9a05aae8
Byron 2016 <bgva2005@yahoo.com>
2021-12-27 10:55:15 -0500

    fix: add userInterfase implementation

---
+++ JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Data/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserDBContext _context;
+
+        public UserRepository(UserDBContext context)
+        {
+            _context = context;
+        }
+        public User Create(User user)
+        {
+            _context.Users.Add(user);
+            user.Id = _context.SaveChanges();
+            return user;
+        }
+    }
+}
