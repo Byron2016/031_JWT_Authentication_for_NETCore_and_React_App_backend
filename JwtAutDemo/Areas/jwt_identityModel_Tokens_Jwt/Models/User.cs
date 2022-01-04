794dec8ec719c1ef77776cab5189adfdac8dc205
Byron 2016 <bgva2005@yahoo.com>
2022-01-03 19:17:21 -0500

    fix(authenticated user): 54.00 hide password from json return information

--- JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Models/User.cs
+++ JwtAutDemo/Areas/jwt_identityModel_Tokens_Jwt/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Models
@@ -10,6 +11,7 @@ namespace JwtAutDemo.Areas.jwt_identityModel_Tokens_Jwt.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        [JsonIgnore] //54.00
         public string Password { get; set; }
     }
 }
